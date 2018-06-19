54049bfb42548adbb27bb04dd66474f74e8f2c4d
amyma17 <amyma171311@gmail.com>
2018-06-19 18:55:06 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -1,8 +1,11 @@
 def simple_curfew_checker(t)
   if t<11
     puts "You'r early"
-end
-
+elsif t=11
+    puts "GO"
+else t>11 
+    puts "You are laaaaate"
+end 
 def curfew_checker(t)
   # code goes here
 end
